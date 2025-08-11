a5e5059e8d23a33e2e7045cdf4abf796230cd7ea
Venrable18 <onenvictor@gmail.com>
2025-08-11 16:21:17 +0100

    still fixing things

--- onsite-program/submissions/week-6-piggy/contracts/PiggyVest.sol
+++ onsite-program/submissions/week-6-piggy/contracts/PiggyVest.sol
@@ -1,146 +1,189 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
-import "../interfaces/ERC20.sol";
+import '../interfaces/ERC20.sol';
 
 contract PiggyVest {
 
-   error NotYourBox();
 
-    error TimeMustBeMoreThanZero();
-
-    error NothingToTake();
-
-    error NoETHSent();
-
-    error TransferFailed();
-
-    struct Saving {
-        address kindOfMoney;
-        uint256 howMuch;
-        uint256 whenStarted;
-        uint256 waitTime;
-    }
-
-    address public saver;
-
-    address public platform;
-
-    uint256 public totalSavings;
-
-    mapping(uint256 => Saving) public allSavings;
-
-    event SavingAdded(uint256 indexed number, address kindOfMoney, uint256 howMuch, uint256 waitTime);
-    event SavingTaken(uint256 indexed number, uint256 gotBack, uint256 feeTaken, bool wasTooSoon);
-
+    error SavingNotFound();
+    error AlreadyWithdrawn();
+    error InvalidPenaltyPercent();
+    error NotOwner();
+    error InvalidTokenAddress();
+    error InvalidAmount();
+    error ETHNotAllowedForERC20();
+    error ERC20TransferFailed();
  
 
-    modifier onlySaver() {
-        if (msg.sender != saver) {
-            revert NotYourBox();
-        }
+    struct Saving {
+        uint256 id;
+        string name;
+        string userName;
+        address userAddress;
+        AssetType assetType;
+        address tokenAddress;
+        uint256 amount;
+        uint256 startTime;
+        uint256 lockPeriod;
+        bool withdrawn;
+    }
+    
+    enum AssetType { ETH, ERC20 }
+
+    address public owner;
+    uint256 public nextSavingId;
+    uint256 public penaltyPercent = 3;
+
+    mapping(address => Saving[]) public userSavings;
+
+
+
+    event SavingCreated(
+        uint256 indexed id,
+        address indexed user,
+        string name,
+        string userName,
+        AssetType assetType,
+        uint256 amount,
+        uint256 lockPeriod
+    );
+
+    event SavingWithdrawn(
+        uint256 indexed id,
+        address indexed user,
+        uint256 amount,
+        bool earlyWithdrawal
+    );
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert NotOwner();
         _;
     }
 
-    constructor(address person, address platformAddress) {
-        saver = person;
-        platform = platformAddress;
+    constructor() {
+        owner = msg.sender;
     }
 
-    function addSaving(address kindOfMoney, uint256 waitTime) external payable onlySaver {
-        if (waitTime == 0) revert TimeMustBeMoreThanZero();
+    function createSaving(
+        string memory _name,
+        string memory _userName,
+        AssetType _assetType,
+        address _tokenAddress,
+        uint256 _amount,
+        uint256 _lockPeriod
+    ) external payable {
+        uint256 amount;
 
-        uint256 moneyIn;
-
-        if (kindOfMoney == address(0)) {
-            if (msg.value == 0) revert NoETHSent();
-            moneyIn = msg.value;
+        if (_assetType == AssetType.ETH) {
+            if (msg.value == 0) revert InvalidAmount();
+            amount = msg.value;
         } else {
-            if (msg.value == 0) revert NoETHSent(); // optional: remove if ETH isn't needed
-            moneyIn = msg.value;
-            bool success = IERC20(kindOfMoney).transferFrom(msg.sender, address(this), moneyIn);
-            if (!success) revert TransferFailed();
+            if (_tokenAddress == address(0)) revert InvalidTokenAddress();
+            if (_amount == 0) revert InvalidAmount();
+            if (msg.value > 0) revert ETHNotAllowedForERC20();
+            IERC20 token = IERC20(_tokenAddress);
+            bool success = token.transferFrom(msg.sender, address(this), _amount);
+            if (!success) revert ERC20TransferFailed();
+            amount = _amount;
         }
 
-        allSavings[totalSavings] = Saving(kindOfMoney, moneyIn, block.timestamp, waitTime);
-        emit SavingAdded(totalSavings, kindOfMoney, moneyIn, waitTime);
-        totalSavings++;
+        Saving memory newSaving = Saving({
+            id: nextSavingId,
+            name: _name,
+            userName: _userName,
+            userAddress: msg.sender,
+            assetType: _assetType,
+            tokenAddress: _tokenAddress,
+            amount: amount,
+            startTime: block.timestamp,
+            lockPeriod: _lockPeriod,
+            withdrawn: false
+        });
+
+        userSavings[msg.sender].push(newSaving);
+        emit SavingCreated(nextSavingId, msg.sender, _name, _userName, _assetType, amount, _lockPeriod);
+        nextSavingId++;
     }
 
-    function takeSaving(uint256 number) external onlySaver {
+    function withdrawSaving(uint256 _id) external {
+        Saving[] storage savings = userSavings[msg.sender];
+        for (uint256 i = 0; i < savings.length; i++) {
+            if (savings[i].id == _id) {
+                if (savings[i].withdrawn) revert AlreadyWithdrawn();
 
-        Saving storage saving = allSavings[number];
+                Saving storage saving = savings[i];
+                saving.withdrawn = true;
 
-        if (saving.howMuch == 0) revert NothingToTake();
+                bool early = block.timestamp < saving.startTime + saving.lockPeriod;
+                uint256 amountToSend = saving.amount;
 
-        bool tooSoon = block.timestamp < saving.whenStarted + saving.waitTime;
+                if (early) {
+                    uint256 penalty = (amountToSend * penaltyPercent) / 100;
+                    amountToSend -= penalty;
+                }
 
-        uint256 fee = tooSoon ? (saving.howMuch * 3) / 100 : 0;
-        
-        uint256 moneyOut = saving.howMuch - fee;
+                if (saving.assetType == AssetType.ETH) {
+                    payable(msg.sender).transfer(amountToSend);
+                } else {
+                    IERC20 token = IERC20(saving.tokenAddress);
+                    token.transfer(msg.sender, amountToSend);
+                }
 
-        saving.howMuch = 0;
-
-        if (saving.kindOfMoney == address(0)) {
-
-            payable(platform).transfer(fee);
-            payable(saver).transfer(moneyOut);
-
-        } else {
-            IERC20(saving.kindOfMoney).transfer(platform, fee);
-            IERC20(saving.kindOfMoney).transfer(saver, moneyOut);
+                emit SavingWithdrawn(_id, msg.sender, amountToSend, early);
+                return;
+            }
         }
 
-        emit SavingTaken(number, moneyOut, fee, tooSoon);
+        revert SavingNotFound();
     }
 
-    function seeSaving(uint256 number) external view returns (
-        address kindOfMoney,
-        uint256 howMuch,
-        uint256 whenStarted,
-        uint256 waitTime,
-        uint256 whenCanTake,
-        bool taken
-    ) {
-
-        Saving memory s = allSavings[number];
-
-        return (s.kindOfMoney, s.howMuch, s.whenStarted, s.waitTime, s.whenStarted + s.waitTime, s.howMuch == 0);
+    function getUserSavings(address _user) external view returns (Saving[] memory) {
+        return userSavings[_user];
     }
 
+    function getSavingBalance(uint256 _id) external view returns (uint256) {
+        Saving[] memory savings = userSavings[msg.sender];
+        for (uint256 i = 0; i < savings.length; i++) {
+            if (savings[i].id == _id) {
+                if (savings[i].withdrawn) revert AlreadyWithdrawn();
+                return savings[i].amount;
+            }
+        }
+        revert SavingNotFound();
+    }
 
-
-    function seeAllETH() external view returns (uint256 totalETH) {
-        for (uint256 i = 0; i < totalSavings; i++) {
-            if (allSavings[i].kindOfMoney == address(0)) {
-                totalETH += allSavings[i].howMuch;
+    function getTotalBalance() external view returns (uint256 total) {
+        Saving[] memory savings = userSavings[msg.sender];
+        for (uint256 i = 0; i < savings.length; i++) {
+            if (!savings[i].withdrawn) {
+                total += savings[i].amount;
             }
         }
     }
 
-
-
-    function seeAllTokens(address kindOfMoney) external view returns (uint256 totalTokens) {
-        for (uint256 i = 0; i < totalSavings; i++) {
-            if (allSavings[i].kindOfMoney == kindOfMoney) {
-                totalTokens += allSavings[i].howMuch;
+    function isMature(uint256 _id) external view returns (bool) {
+        Saving[] memory savings = userSavings[msg.sender];
+        for (uint256 i = 0; i < savings.length; i++) {
+            if (savings[i].id == _id) {
+                return block.timestamp >= savings[i].startTime + savings[i].lockPeriod;
             }
         }
+        revert SavingNotFound();
     }
 
-
-
-    function howManySavings() external view returns (uint256) {
-        return totalSavings;
+    function getMaturityDate(uint256 _id) external view returns (uint256) {
+        Saving[] memory savings = userSavings[msg.sender];
+        for (uint256 i = 0; i < savings.length; i++) {
+            if (savings[i].id == _id) {
+                return savings[i].startTime + savings[i].lockPeriod;
+            }
+        }
+        revert SavingNotFound();
     }
 
-
-
-    receive() external payable {
-        revert("Use addSaving to put ETH");
-    }
-
-    fallback() external payable {
-        revert("Wrong call");
+    function changePenaltyPercent(uint256 _newPercent) external onlyOwner {
+        if (_newPercent > 100) revert InvalidPenaltyPercent();
+        penaltyPercent = _newPercent;
     }
 }
